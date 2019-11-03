bb7181fd13809997726816f6dd9739c9428cfc14
Lucas Santos <lhs.santoss@gmail.com>
2019-11-02 23:11:38 -0300

    Fix update and create

--- src/domain/group/Group.ts
+++ src/domain/group/Group.ts
@@ -1,6 +1,6 @@
 import { ObjectId } from 'bson'
 import { BaseEntity, BaseEntityData } from '../BaseEntity'
-import { CreateGroupData } from './structures/CreateGroupData'
+import { CreateGroupData, LocationObject } from './structures/CreateGroupData'
 
 export type PictureObject = {
   profile: string
@@ -18,22 +18,26 @@ export class Group extends BaseEntity {
     profile: '',
     banner: ''
   }
+  location: LocationObject = {
+    city: '',
+    state: '',
+    country: ''
+  }
   socialNetworks: SocialNetworkObject[] = []
   followers: ObjectId[] = []
   tags: string[] = []
-  events: ObjectId[] = []
 
   static create (id: ObjectId, data: CreateGroupData & BaseEntityData): Group {
     const group = new Group()
     group.id = id
     group.name = data.name
     group.founder = new ObjectId(data.founder)
-    group.organizers = data.organizers ? data.organizers.map(organizer => new ObjectId(organizer)) : []
-    group.pictures = data.pictures
     group.socialNetworks = data.socialNetworks
-    group.tags = []
-    group.events = []
 
+    if (data.organizers) group.organizers = data.organizers.map(organizer => new ObjectId(organizer))
+    if (data.pictures) group.pictures = data.pictures
+    if (data.tags) group.tags = data.tags
+    if (data.location) group.location = data.location
     if (data.createdAt) group.createdAt = data.createdAt
     if (data.updatedAt) group.updatedAt = data.updatedAt
     if (data.deletedAt) group.deletedAt = data.deletedAt
@@ -41,14 +45,14 @@ export class Group extends BaseEntity {
     return group
   }
 
-  update (dataToUpdate: CreateGroupData) {
-    this.name = dataToUpdate.name
-    this.founder = new ObjectId(dataToUpdate.founder)
-    this.organizers = dataToUpdate.organizers ? dataToUpdate.organizers.map(organizer => new ObjectId(organizer)) : []
-    this.pictures = dataToUpdate.pictures
-    this.socialNetworks = dataToUpdate.socialNetworks
-    this.tags = []
-    this.events = []
+  update (dataToUpdate: Partial<CreateGroupData>) {
+    this.name = dataToUpdate.name || this.name
+    this.founder = dataToUpdate.founder ? new ObjectId(dataToUpdate.founder) : this.founder
+    this.organizers = dataToUpdate.organizers ? dataToUpdate.organizers.map(organizer => new ObjectId(organizer)) : this.organizers
+    this.pictures = dataToUpdate.pictures ? dataToUpdate.pictures : this.pictures
+    this.socialNetworks = dataToUpdate.socialNetworks || this.socialNetworks
+    this.location = dataToUpdate.location ? { ...this.location, ...dataToUpdate.location } : this.location
+    this.tags = dataToUpdate.tags || this.tags
     this.updatedAt = new Date()
     return this
   }
@@ -65,7 +69,7 @@ export class Group extends BaseEntity {
       },
       socialNetworks: this.socialNetworks,
       tags: this.tags,
-      events: this.events,
+      location: this.location,
       createdAt: this.createdAt,
       updatedAt: this.updatedAt,
       deletedAt: this.deletedAt

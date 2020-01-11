f9c392cd4a0b3222563251dc7a52a850357d17e9
italo josé <italo.i@live.com>
2020-01-11 00:21:44 -0300

    update tests

--- test/mocks/groups.ts
+++ test/mocks/groups.ts
@@ -10,7 +10,6 @@ validGroup.name= 'typescriptbr',
 validGroup.description= 'sadsadsa',
 validGroup.founder = new ObjectId('5ddbe2b325f79200107a080e'),
 validGroup.organizers= [],
-validGroup.followers= [],
 validGroup.slug= 'typescriptbr',
 validGroup.pictures = {
     profile: 'https=//chegaai.blob.core.windows.net/groups/5ddbe5bfe791c25971081cba',
@@ -42,5 +41,18 @@ const createGroupData = {
   location: validGroup.location
 }
 
-export { validGroup, createGroupData, VALID_GROUP_OBJECT_ID }
+const updateGroupData = {
+  name: validGroup.name,
+  description: validGroup.description,
+  pictures: {
+    profile:'base64',
+    banner: 'base64'
+  },
+  socialNetworks: validGroup.socialNetworks,
+  tags: validGroup.tags,
+  organizers: [new ObjectId('5e1930713af2d8471df20487'), new ObjectId('5e1930a57acc1c1d2b8c369a')],
+  location: validGroup.location
+}
+
+export { validGroup, createGroupData, VALID_GROUP_OBJECT_ID, updateGroupData }
 
